996fe3628383c4ab87cc56a59837b07f191f7670
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-11-02 04:41:03 +0200

    chore: removed drizzle stuff

--- data/env/server.ts
+++ data/env/server.ts
@@ -3,20 +3,12 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    DB_PASSWORD: z.string().min(1),
-    DB_USER: z.string().min(1),
-    DB_NAME: z.string().min(1),
-    DB_HOST: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
     CLERK_WEBHOOK_SIGNING_SECRET: z.string().min(1),
     TEST_IP_ADDRESS: z.string().min(1).optional(),
     CLERK_JWT_ISSUER_DOMAIN: z.string().min(1),
   },
   runtimeEnv: {
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_USER: process.env.DB_USER,
-    DB_NAME: process.env.DB_NAME,
-    DB_HOST: process.env.DB_HOST,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     CLERK_WEBHOOK_SIGNING_SECRET: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
     TEST_IP_ADDRESS: process.env.TEST_IP_ADDRESS,

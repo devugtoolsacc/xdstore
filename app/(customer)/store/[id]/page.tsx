0204ee1d983a4e4ba8ce1e854f92dbe293ed31c9
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-11-04 02:58:25 +0200

    feat: store view

--- app/(customer)/store/[id]/page.tsx
+++ app/(customer)/store/[id]/page.tsx
@@ -3,7 +3,7 @@ import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { Suspense } from 'react';
 import LoadingSpinner from '@/app/components/LoadingSpinner';
-import StoreView from '@/features/users/schemas/customer/stores/store/components/StoreView';
+import StoreView from '@/features/users/schemas/customer/store/components/StoreView';
 import { notFound } from 'next/navigation';
 
 export default async function StorePage({
